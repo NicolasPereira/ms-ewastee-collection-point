7544070e6b0750b0d0149814839f5b2b1e5afe4e
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-11-03 01:56:08 -0300

    feat: create migrate collection points

---
+++ database/migrations/2022_11_03_021729_create_collection_point_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collection_points', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('cep');
+            $table->string('logradouro');
+            $table->string('numero');
+            $table->string('complemento');
+            $table->string('bairro');
+            $table->string('localidade');
+            $table->string('uf');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collection_points');
+    }
+};
